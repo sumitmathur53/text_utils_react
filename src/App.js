1608526d42ea067a96baa6c2c6ad604f84cf0229
sumit <sumitmathur170@gmail.com>
2023-06-28 15:35:09 +0530

    lec 12 improving the dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+// import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForms from "./components/TextForms";
 // import About from "./components/About";
+import React, { useState } from 'react';  // imrs to import
+
 
 function App() {
+
+  const[mode, setMode] = useState('light');  // wheather the dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+
   return (
     <>
-      <Navbar title="textutils" aboutText="About" />
+      {/* <Navbar title="textutils" aboutText="About" mode = {mode} /> */}
+      
+      <Navbar title="textutils" mode = {mode} toggleMode={toggleMode}/>
 
       <div className="container my-3">
-        <TextForms heading = "Enter the text to analyse"/>
+        <TextForms heading = "Enter the text to analyse" mode={mode}/>
       </div>
 
       {/* <About/> */}
